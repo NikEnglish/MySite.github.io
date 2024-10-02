46a9ef4fe464b930de51e42e2be7bb17563df758
NikEnglish <plna1867ss@gmail.com>
2024-10-02 17:18:50 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,31 @@
 // script.js
 
 document.addEventListener('DOMContentLoaded', () => {
-    const heroText = document.querySelector('.hero-text');
+    const navLinks = document.querySelectorAll('nav ul li a');
 
-    function animateText() {
-        heroText.style.opacity = '1';
-        heroText.style.transform = 'translate(-50%, -50%) scale(1)';
-        
-        setTimeout(() => {
-            heroText.style.transitionProperty = 'opacity transform scale';
-            heroText.style.transitionDuration = '0.5s';
-            
-            setTimeout(() => {
-                heroText.style.transitionProperty = '';
-                heroText.style.transitionDuration = '';
-            }, 500);
-        }, 1000);
-    }
+    navLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetSectionId = link.getAttribute('href');
+            const targetSection = document.querySelector(targetSectionId);
 
-    function animateOut() {
-        heroText.style.opacity = '0';
-        heroText.style.transform = 'translate(-50%, -50%) scale(0.95)';
-        heroText.style.transitionProperty = 'opacity transform scale';
-        heroText.style.transitionDuration = '0.5s';
-        
-        setTimeout(() => {
-            heroText.style.display = 'none';
-        }, 500);
-    }
-
-    window.addEventListener('scroll', () => {
-        const scrollPosition = window.scrollY;
-        const viewportHeight = window.innerHeight;
-
-        if (window.innerHeight > document.body.offsetHeight) {
-            const bottomOfPage = document.body.offsetHeight - window.innerHeight;
-            if (bottomOfPage <= scrollPosition) {
-                animateOut();
-            } else {
-                heroText.style.opacity = '0';
-                heroText.style.transform = 'translate(-50%, -50%) scale(0.95)';
-                heroText.style.display = 'none';
-            }
-        } else {
-            if (scrollPosition >= window.innerHeight * 0.75) {
-                animateText();
-            }
-        }
+            // Smooth scrolling
+            targetSection.scrollIntoView({ behavior: 'smooth' });
+        });
     });
+
+    // Add smooth scrolling effect to all links with hashes
+    (function() {
+        'use strict';
+        var anchorLinks = document.querySelectorAll('a[href^="#"]');
+        for (var i = 0; i < anchorLinks.length; i++) {
+            anchorLinks[i].addEventListener('click', function(e) {
+                var targetID = this.getAttribute('href');
+                e.preventDefault();
+                document.getElementById(targetID).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        }
+    })();
 });
